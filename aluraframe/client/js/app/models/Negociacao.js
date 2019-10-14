3e403f2563bf6b150cf9bb424532a48fe5efc494
tpraissa <raissatelep@gmail.com>
2019-10-14 16:10:23 -0300

    objetos imutaveis

--- aluraframe/client/js/app/models/Negociacao.js
+++ aluraframe/client/js/app/models/Negociacao.js
@@ -1,8 +1,27 @@
 class Negociacao {
 
     constructor(data, quantidade, valor){
-        this.data = data;
-        this.quantidade = quantidade;
-        this.valor = valor;
+        this._data = data;
+        this._quantidade = quantidade;
+        this._valor = valor;
+        Object.freeze(this);
+    }
+
+    get data(){
+        return this._data;
+    }
+
+    get quantidade(){
+        return this._quantidade;
+    }
+
+    get valor(){
+        return this._valor;
+    }
+
+
+
+    get volume(){
+        return this._quantidade * this._valor;
     }
 }
